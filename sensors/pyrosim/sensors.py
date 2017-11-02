cfe0705e0bf21cbbdbfc3ce01545598a594c4541
Dipen Ved <vedipen@gmail.com>
2017-11-02 14:57:12 +0530

    Added Proprioceptive sensor

--- sensors/pyrosim/sensors.py
+++ sensors/pyrosim/sensors.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from pyrosim import PYROSIM
 sim = PYROSIM(playPaused=False, evalTime=100)
 sim.Send_Cylinder( objectID=0, x=0 , y=0 , z=0.6 , length=1.0 , radius=0.1 )
@@ -5,7 +6,16 @@ sim.Send_Cylinder( objectID=1 , x=0 , y=0.5 , z=1.1 , r=1 , g=0 , b=0 , r1=0 , r
 sim.Send_Joint( jointID=0 , firstObjectID=0 , secondObjectID=1 , x=0 , y=0 , z=1.1 , n1=-1 , n2=0 , n3=0 , lo=-3.14159/2 , hi=3.14159/2 );
 sim.Send_Touch_Sensor( sensorID = 0 , objectID = 0 )
 sim.Send_Touch_Sensor( sensorID = 1 , objectID = 1 )
+sim.Send_Proprioceptive_Sensor(sensorID=2, jointID=0)
 sim.Start()
 sim.Wait_To_Finish()
-sensorData = sim.Get_Sensor_Data(sensorID = 1)
-print sensorData
+sensorData0 = sim.Get_Sensor_Data(sensorID = 0)
+sensorData1 = sim.Get_Sensor_Data(sensorID = 1)
+sensorData2 = sim.Get_Sensor_Data(sensorID = 2)
+f = plt.figure()
+pn = f.add_subplot(111)
+plt.plot(sensorData0)
+plt.plot(sensorData1)
+plt.plot(sensorData2)
+pn.set_ylim(-2,+2)
+plt.show()
